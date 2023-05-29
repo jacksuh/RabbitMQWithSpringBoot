42c5b1533a095fd943817334d3039773aabc4878
jacksuh <jacksonsdss@gmail.com>
2023-05-29 12:12:02 -0300

    Projeto rabbit controller

--- src/main/java/com/api/rabbitmq/fila/TesteController.java
+++ src/main/java/com/api/rabbitmq/fila/TesteController.java
@@ -1,5 +1,8 @@
 package com.api.rabbitmq.fila;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/teste")
 public class TesteController {
 
-    @Autowired
-    private QueueSender queueSender;
+    public TesteController(AmqpTemplate queueSender) {
+        this.queueSender = queueSender;
+    }
+
+    private final AmqpTemplate queueSender;
 
     @GetMapping
     public String send(){
-        queueSender.send("test message");
+
+        String mensagem = "test message";
+
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setHeader("ultima", "sim");
+        Message message = new Message(mensagem.getBytes(), messageProperties);
+
+        queueSender.convertAndSend("teste-exchange", "routing-key-teste", message);
         return "ok. done";
     }
 }
