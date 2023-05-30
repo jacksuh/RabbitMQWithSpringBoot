7d2aa23b8a2c7640fac6ec7c62ddf841725adfbb
jacksuh <jacksonsdss@gmail.com>
2023-05-30 18:04:53 -0300

    Projeto configurações

--- src/main/java/com/api/rabbitmq/fila/QueueConsumer.java
+++ src/main/java/com/api/rabbitmq/fila/QueueConsumer.java
@@ -1,14 +1,25 @@
 package com.api.rabbitmq.fila;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class QueueConsumer {
 
     @RabbitListener(queues = {"${queue.name}"})
-    public void receive(@Payload String fileBody) {
-        System.out.println("Message " + fileBody);
+    public void receive(@Payload Message message) throws BusinessException {
+        System.out.println("Message " + message + "  " + LocalDateTime.now());
+        String ultima = String.valueOf(message.getHeaders().get("ultima"));
+        if(ultima.equals("sim")){
+            System.out.println(ultima);
+        }
+        String payload = String.valueOf(message.getPayload());
+        if(payload.equals(1)) {
+            throw new BusinessException("testando a excecao");
+        }
     }
 }
